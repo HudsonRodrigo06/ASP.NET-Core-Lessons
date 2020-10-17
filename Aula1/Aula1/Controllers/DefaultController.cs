f1fff9c7981aa4cdd986bbcea446c0b99a6c19cb
Hudson <hudsonrodrigo06@gmail.com>
2020-10-17 08:06:31 -0300

    Controller and Routes
    Ao acessar uma rota ( ex: /login ), é direcionado para o controler de mesmo nome ( LoginController ) que possui uma 'Action' ( função ) com um nome ( ex: public classIndex() ) e o mecanismo ASP.NET retona uma view, onde o mesmo busca dentro da pasta "View" por uma pasta chamada "Login" cujo conteudo possua um arquivo com o mesmo nome da 'Action' ( ex: Index.cshtml ).

---
+++ Aula1/Aula1/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula1.Controllers
+{
+	public class DefaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Portal()
+		{
+			return View();
+		}
+	}
+
+}
