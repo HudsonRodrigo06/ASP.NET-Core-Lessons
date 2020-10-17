e02d8963b537ff9203c8ec66ce54b94ec21a6bff
Hudson <hudsonrodrigo06@gmail.com>
2020-10-17 09:16:07 -0300

    Pratiacndo conteúdo aprendido na Aula 1
    Conceitos de Controller, Actions e ViewBag.

---
+++ Pratice - Aula1/pratice-Aula1/pratice-Aula1/Controllers/PrincipalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pratice_Aula1.Controllers
+{
+	public class PrincipalController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View("Principal");
+		}
+
+		public IActionResult Logar(string user, string pwd)
+		{
+			if (user == "123")
+				ViewBag.Msg = "Login efetuado com sucesso!";
+			else
+				ViewBag.Msg = "Dados inválidos!";
+
+			return View("Principal");
+		}
+	}
+}
---
+++ Pratice - Aula1/pratice-Aula1/pratice-Aula1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pratice_Aula1.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
