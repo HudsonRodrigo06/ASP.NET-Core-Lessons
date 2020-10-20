9323b23295c3d845da33a4bb20d4364bea0082d4
Hudson <hudsonrodrigo06@gmail.com>
2020-10-20 20:10:19 -0300

    JS layout elements

--- Aula1/Aula1/wwwroot/js/Views/Login/Index.js
+++ Aula1/Aula1/wwwroot/js/Views/Login/Index.js
@@ -5,37 +5,44 @@
 // utilizando objeto como contexto para proteger
 let index = {
 
-	logar: function () {
+	errorMsg: function () {
+		Swal.fire({
+			icon: 'error',
+			title: 'Oops...',
+			text: 'Dados inválidos!'
+		})
+	},
 
-		var email = document.getElementById("email");
-		var senha = document.getElementById("senha");
+	sucessMsg: function () {
+		Swal.fire(
+			'Good job!',
+			'Login efetuado com sucesso!',
+			'success'
+		)
 
-		if (email.value.trim() == "" || senha.value.trim() == "") {
-			Swal.fire({
-				icon: 'error',
-				title: 'Oops...',
-				text: 'Dados inválidos!'
-			})
-
-			email.focus();
-		}
-		else {
-			/*
+		/*
 				Swal.fire({
 				position: 'top-end',
 				icon: 'success',
 				title: 'Your work has been saved',
 				showConfirmButton: false,
 				timer: 1500
-			})
-			*/
+				})
+		*/
+	},
 
-			Swal.fire(
-				'Good job!',
-				'Login efetuado com sucesso!',
-				'success'
-			)
+	logar: function () {
 
+		var email = document.getElementById("email");
+		var senha = document.getElementById("senha");
+
+		
+
+		if (email.value.trim() == "" || senha.value.trim() == "") {
+			this.errorMsg();
+		}
+		else {
+			this.sucessMsg();
 			//redirect
 		}
 
